035fd628de151022d2164f0f1365b8034a18d12b
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-02-03 05:28:59 +0100

    Закінчив роботу з рецептами

--- src/pages/RecipePage.tsx
+++ src/pages/RecipePage.tsx
@@ -1,30 +1,31 @@
 import {Link, useParams} from "react-router-dom";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {RecipeDetails} from "../components/recipes/RecipeDetails.tsx";
 import {RecipeAuthor} from "../components/recipes/RecipeAuthor.tsx";
-import {useAppSelector} from "../redux/hooks/useAppSelector.ts";
 import {IRecipe} from "../models/recipe/IRecipe.ts";
-import {useAppDispatch} from "../redux/hooks/useAppDispatch.ts";
-import {fetchRecipes} from "../redux/slices/recipesSlice.ts";
 
 export const RecipePage = () => {
     const {recipeId} = useParams<{recipeId: string}>();
-    const dispatch = useAppDispatch();
-    const {recipes, loading} = useAppSelector(state => state.recipes);
+    const [recipe, setRecipe] = useState<IRecipe | null>(null);
 
     useEffect(() => {
-        dispatch(fetchRecipes({limit: 10, page: 1}));
-    }, [dispatch]);
+        const localStorageRecipes = localStorage.getItem("allRecipes");
 
-    const recipe = recipes.find((recipe: IRecipe) => recipe.id === Number(recipeId));
-
-    if (loading) return <div>Завантаження...</div>;
-    if (!recipe) return <div>Рецепт не знайдено</div>;
+        if (localStorageRecipes) {
+            const recipes = JSON.parse(localStorageRecipes);
+            const foundRecipe = recipes.find((recipe: IRecipe) => recipe.id === Number(recipeId));
+            setRecipe(foundRecipe || null);
+        }
+    }, [recipeId]);
 
     return (
         <div>
-            <RecipeDetails recipe={recipe}/>
-            <RecipeAuthor authorId={recipe.userId}/>
+            {recipe && (
+                <>
+                    <RecipeDetails recipe={recipe} />
+                    <RecipeAuthor authorId={recipe.userId} />
+                </>
+            )}
             <Link to={"/auth/recipes"}>Назад до списку рецептів</Link>
         </div>
     );
